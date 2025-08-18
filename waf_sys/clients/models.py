109c621afd7a6e1fae7c5f3385809373f03326b0
Oliyad  Teshome <159255834+Ethioli@users.noreply.github.com>
2025-08-18 20:43:36 +0300

    Update models.py
    adding tables for the clients web and for the users

--- waf_sys/clients/models.py
+++ waf_sys/clients/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#model for the clients info
+class Client(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    api_key = models.CharField(max_length=255, unique=True)
+    target_url = models.URLField() 
+
+    def __str__(self):
+        return self.name
+#model for the clients web owner
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.client}"
