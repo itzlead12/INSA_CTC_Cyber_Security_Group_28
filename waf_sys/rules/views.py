27184b3ffcc6fbf8389076774c393d85790de943
Oliyad  Teshome <159255834+Ethioli@users.noreply.github.com>
2025-08-19 21:23:42 +0300

    adding all the views views.py
    Implemented views for managing WAF rules and exposing APIs:
    
    1, rules_list: display all rules with client info.
    2, rules_create: create new rules via form.
    3,  api_rules: return active rules for a client based on api_key.
    4, api_log_blocked_request: log blocked requests from agents.
    
    Includes form handling, JSON response support, and validation with api_key.

--- waf_sys/rules/views.py
+++ waf_sys/rules/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.views.decorators.http import require_GET, require_POST
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from customers.models import Client
+from .models import WAFRule, BlockedRequest
+from .forms import WAFRuleForm
+import json
 
-# Create your views here.
+# to list rules
+@login_required
+def rules_list(request):
+    rules = WAFRule.objects.select_related("client").all()
+    return render(request, "rules_list.html", {"rules": rules})# templates will be created
+
+@login_required
+def rules_create(request):
+    if request.method == "POST":
+        form = WAFRuleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("rules:rules_list")
+    else:
+        form = WAFRuleForm()
+    return render(request, "rules_create.html", {"form": form})
+
+@require_GET
+def api_rules(request):
+    api_key = request.GET.get("api_key")
+    if not api_key:
+        return HttpResponseBadRequest("Missing api_key")
+    try:
+        client = Client.objects.get(api_key=api_key)
+    except Client.DoesNotExist:
+        return HttpResponseBadRequest("Invalid api_key")
+
+    rules = list(
+        WAFRule.objects.filter(client=client, is_active=True).values("rule_type", "value")
+    )
+    return JsonResponse({"client": client.name, "target_url": client.target_url, "rules": rules})
+
+@csrf_exempt
+@require_POST
+def api_log_blocked_request(request):
+    try:
+        data = json.loads(request.body.decode("utf-8"))
+        api_key = data.get("api_key")
+        client = Client.objects.get(api_key=api_key)
+        BlockedRequest.objects.create(
+            client=client,
+            ip_address=data.get("ip_address","0.0.0.0"),
+            request_path=data.get("request_path",""),
+            user_agent=data.get("user_agent",""),
+            reason=data.get("reason",""),
+        )
+        return JsonResponse({"status": "ok"})
+    except Client.DoesNotExist:
+        return HttpResponseBadRequest("Invalid api_key")
+    except Exception as e:
+        return HttpResponseBadRequest(f"Error: {e}")
