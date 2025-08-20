a8850a69091f4d5e6fe9e4a803f171ddeee842d7
Oliyad  Teshome <159255834+Ethioli@users.noreply.github.com>
2025-08-20 22:40:19 +0300

    Update Client model with host field and improved target_url models.py
    Modified Client model:
    - Replaced api_key with host field to store client domain.
    - Added help_text for host and target_url for better clarity.
    - Set default value for host.
    
    And Profile model is not touched

--- waf_sys/clients/models.py
+++ waf_sys/clients/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#model for the clients info
 class Client(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    api_key = models.CharField(max_length=255, unique=True)
-    target_url = models.URLField() 
+    host = models.CharField(max_length=255, unique=True, help_text="The client's domain (e.g., example.com)", default="example.com")
+    target_url = models.URLField(help_text="The origin server (e.g., https://webserver.internal)")
 
     def __str__(self):
         return self.name
-#model for the clients web owner
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True, blank=True)
