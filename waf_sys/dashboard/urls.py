04a247a583ed48bfea7da240f70dd9fe19a43c03
Oliyad  Teshome <159255834+Ethioli@users.noreply.github.com>
2025-08-29 22:16:08 +0300

    Update urls.py
    As the views was updated the urls must be 
    url for the login and client_list instead of the client dashboard because the client_dashboard should be int the clients app

--- waf_sys/dashboard/urls.py
+++ waf_sys/dashboard/urls.py
@@ -6,7 +6,7 @@ app_name = "dashboard"
 
 urlpatterns = [
     path("", views.admin_dashboard, name="admin_dashboard"),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', views.login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path("client/", views.client_dashboard, name="client_dashboard"),
+    path("clients/", views.client_list, name="client_list"),
 ]
