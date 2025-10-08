e610ce6df51db98f95c350c81b03523709afc57b
Oliyad  Teshome <159255834+Ethioli@users.noreply.github.com>
2025-10-08 06:12:41 -1200

    Update admin.py

--- waf_sys/clients/admin.py
+++ waf_sys/clients/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Client,ClientRuleSet
 
 # Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'host', 'target_url', 'is_active', 'created_at']
+    list_filter = ['is_active', 'created_at']
+    search_fields = ['name', 'host']
+
+admin.site.register(ClientRuleSet)
