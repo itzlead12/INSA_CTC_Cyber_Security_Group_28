2d907a0d2b9d3c9fe4c5f7505cfca9b202851cdd
Oliyad  Teshome <159255834+Ethioli@users.noreply.github.com>
2025-10-09 02:46:11 -1200

    i added for the dashboard view and for the api urls.py

--- Core/dashboard/urls.py
+++ Core/dashboard/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('xz3a21rqhe4oo6ox2w31rryga/', views.admin_dashboard, name='admin_dashboard'),
+    path('api/stats/', views.api_dashboard_stats, name='api_dashboard_stats'),
    
 ]
